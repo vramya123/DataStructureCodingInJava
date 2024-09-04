5df99296ed03d7689ca227e7b1872c10f44eae07
vramya123 <rudiprocks@yahoo.co.in>
2024-09-04 16:28:55 +0530

    Create CountDigitsInNumber.java

---
+++ Numbers/CountDigitsInNumber.java
@@ -0,0 +1,43 @@
+/*
+ Count the number of Digits 
+ TC :- O(n) where n is the number of digits
+ */
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class CountDigitsInNumber{
+    
+    
+    public static int findTheNumberOfDigitsInGivenNumber(int num){
+        
+        int noOfDigits = 0;
+        
+        while( num > 0 ){
+            
+            //Removes the last digit in the number, quotient is assigned to num
+            num = num /10;
+            
+            noOfDigits++;
+        }
+        
+        return noOfDigits;
+    }
+    
+    
+    public static void main(String[] args){
+        
+        int num = 5678;
+        
+        int noOfDigits = findTheNumberOfDigitsInGivenNumber(num);
+        
+        System.out.println("the no of digits for "+ num +" is:"+noOfDigits);
+    }
+    
+    
+    
+}
+
+//o/p:- the no of digits for 5678 is:4
