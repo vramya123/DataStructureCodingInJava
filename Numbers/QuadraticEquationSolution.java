ea4139cae7a9d00b704429368081f88ac8f5f837
vramya123 <rudiprocks@yahoo.co.in>
2024-10-08 18:51:38 +0530

    Update QuadraticEquationSolution.java

--- Numbers/QuadraticEquationSolution.java
+++ Numbers/QuadraticEquationSolution.java
@@ -58,3 +58,49 @@ public class QuadraticEquationSolution{
 The root is :-1.0
 The root is :-1.5
 */
+
+/*  Varaiation from GFG
+
+Given a quadratic equation ax2 + bx + c = 0, find its roots. If the roots are imaginary, return only one integer -1. Always return the roots as the greatest integers less than or equal to the actual roots, with the maximum root first followed by the minimum root.
+
+Note: If roots are imaginary, the generated output will display "Imaginary"
+
+*/
+
+
+
+// User function Template for Java
+
+class Solution {
+    public ArrayList<Integer> quadraticRoots(int a, int b, int c) {
+       ArrayList<Integer> res = new ArrayList<>();
+        Integer d = (b*b) - (4*a*c);
+        
+        if(d == 0){
+            
+            
+            Integer r1 = -b/ (2 * a);
+            res.add(r1);
+            res.add(r1);
+            
+            
+        }else if (d > 0){
+            
+            double dsqrt = Math.sqrt(d);
+            Integer r1 =   (int) Math.floor((-b + dsqrt ) / (2*a));
+            Integer  r2 =  (int) Math.floor((-b - dsqrt) / (2*a));
+            if( r1 > r2){
+                 res.add(r1);
+                 res.add(r2);
+            }else{
+                res.add(r2);
+                res.add(r1);
+            } 
+        }else{
+            
+             res.add(-1);
+        }
+     
+     return res;   
+    }
+}
