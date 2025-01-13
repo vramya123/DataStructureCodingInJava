f5232504df48a88a3a61ef9fd024753fa376a6b5
vramya123 <rudiprocks@yahoo.co.in>
2025-01-13 16:44:39 +0530

    Update ReverseArrayUsingRecursion.java

--- Recursion/ReverseArrayUsingRecursion.java
+++ Recursion/ReverseArrayUsingRecursion.java
@@ -11,11 +11,17 @@ import java.util.*;
 
 public class ReverseArrayUsingRecursion{
     
-     public static void reverseTheGivenArray(int[] a){
+     public static void reverseTheGivenArrayUsingTwoPointer(int[] a){
          
          reverse(a,0,a.length-1);
      }
      
+      public static void reverseTheGivenArrayUsingOnePointer(int[] a){
+         
+         reverse1(a,0,a.length);
+     }
+     
+     //Two pointer approach 
      public static void reverse(int[] a, int l, int h){
          //base case
          
@@ -26,6 +32,33 @@ public class ReverseArrayUsingRecursion{
          reverse(a,l+1,h-1);
      }
      
+     
+     //One pointer approach
+     
+     /*
+     
+     RT
+     
+     f(a,0,6)
+     
+     f(a,1,6)
+     
+     f(a,2,6)
+     
+     f(a,3,6)
+     
+     */
+     public static void reverse1(int[] a, int i, int n){
+         
+         //Base case
+         if( i >= n/2){
+             return;
+         }
+          swap(a,i,n-i-1);
+          reverse1(a, i+1, n); //When i  used i++ , it resulted in stackoverflow 
+         
+     }
+     
      public static void swap(int[] a, int l, int h){
          int tmp = a[l];
          a[l] = a[h];
@@ -34,7 +67,13 @@ public class ReverseArrayUsingRecursion{
      
      public static void main(String[] args){
          int[] a = {2,3,4,1,5,6};
-         reverseTheGivenArray(a);
+         reverseTheGivenArrayUsingTwoPointer(a);
+         for(int i=0;i<a.length;i++){
+             System.out.print(a[i]+" ");
+         }
+         System.out.println();
+         
+         reverseTheGivenArrayUsingOnePointer(a);
          for(int i=0;i<a.length;i++){
              System.out.print(a[i]+" ");
          }
@@ -42,3 +81,4 @@ public class ReverseArrayUsingRecursion{
 }
 
 //o/p:- 6 5 1 4 3 2 
+// 2 3 4 1 5 6 
