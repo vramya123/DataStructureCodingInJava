8beb13ab0485f161088671d8a0f203241d311bec
vramya123 <rudiprocks@yahoo.co.in>
2025-06-06 17:56:27 +1000

    Update CountNoOfSubsetWithGivenDiff.java

--- DynamicProgramming/AditiyaVerma/CountNoOfSubsetWithGivenDiff.java
+++ DynamicProgramming/AditiyaVerma/CountNoOfSubsetWithGivenDiff.java
@@ -1,21 +1,11 @@
 /*
-Count the number of subset with a given difference
 
 youtube:- https://www.youtube.com/watch?v=ot_XBHyqpFc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=11
 
-*/
+=> Count the number of subset with a given difference
 
-
-import java.io.*;
-import java.util.*;
-
-
-class CountNoOfSubsetWithGivenDiff{
-    
-    
-    /*
-    
-    arr =>  subset1(s1)  - subset2(s2) = diff
+=> Solution:- 
+ arr =>  subset1(s1)  - subset2(s2) = diff
     
     eq1  =>  s1 - s2 = diff
     eq2 =>   s1 + s2 = summation
@@ -25,11 +15,48 @@ class CountNoOfSubsetWithGivenDiff{
                
     
     Now we have reduce the problem to count the number of subset whose sum is equal to s1
-    
-    
-    */
-    
-    
+
+*********Another way of asking the same question is "target sum"
+
+=> You are given an integer array nums and an integer target.
+
+You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+Return the number of different expressions that you can build, which evaluates to target.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,1,1], target = 3
+Output: 5
+Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+
+
+=> Solution
+
+ arr =  +1 + 1 + 1 + 1 - 1
+ => s1 = (+1+1+1) s2 = (-1)
+ => s1 - s2 = diff/target
+    (+1+1+1)-(1) = 3
+     
+Same as counting the no of subset whose diff is equal to the one given
+
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+
+class CountNoOfSubsetWithGivenDiff{
+
     public int countTheNoOfSubsetWithTheGivenDiff(int[] arr, int diff){
         
         //Find the summation
